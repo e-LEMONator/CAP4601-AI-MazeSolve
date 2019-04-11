9167caf6aace48e262366b7588d49e0168fddde9
jms164 <jms164@students.uwf.edu>
2019-04-10 19:32:49 -0500

    refactored printer

--- src/com/mazesolver/java/utilities/MazePrinter.java
+++ src/com/mazesolver/java/utilities/MazePrinter.java
@@ -6,42 +6,63 @@ public class MazePrinter
 {
 	public static void printMaze(Maze maze)
 	{
+		printFirstLine(maze);
+		
+		for(int row = 0; row < Constants.getMazeSize(); row++)
+		{
+			printCellLine(maze, row);
+			
+			printWallLine(maze, row);
+		}
+		
+		System.out.printf("\n\n");
+	}
+	
+	private static void printFirstLine(Maze maze)
+	{
 		MazeCell currentCell;
 		
-		System.out.printf("+");
+		System.out.printf("#");
 			
 		for(int column = 0; column < Constants.getMazeSize(); column++)
 		{
 			currentCell = maze.getMazeGrid()[0][column];
 
-			System.out.printf("%c%c", ((currentCell.getUpEdge().get() < 1) ? '-' : ' '), '+');
+			System.out.printf("%c%c", ((currentCell.getUpEdge().get() < 1) ? '-' : ' '), '#');
 		}
 
 		System.out.printf("\n");
+	}
+	
+	private static void printCellLine(Maze maze, int row)
+	{
+		MazeCell currentCell;
 		
-		for(int row = 0; row < Constants.getMazeSize(); row++)
+		System.out.printf("|");
+			
+		for(int column = 0; column < Constants.getMazeSize(); column++)
 		{
-			System.out.printf("|");
-			
-			for(int column = 0; column < Constants.getMazeSize(); column++)
-			{
-				currentCell = maze.getMazeGrid()[row][column];
-				
-				System.out.printf("%c%c", currentCell.getSymbol(), ((currentCell.getRightEdge().get() < 1) ? '|' : ' '));
-			}
-			
-			System.out.printf("\n+");
-			
-			for(int column = 0; column < Constants.getMazeSize(); column++)
-			{
-				currentCell = maze.getMazeGrid()[row][column];
-				
-				System.out.printf("%c%c", ((currentCell.getDownEdge().get() < 1) ? '-' : ' '), '+');
-			}
-			
-			System.out.printf("\n");
+			currentCell = maze.getMazeGrid()[row][column];
+
+			System.out.printf("%c%c", currentCell.getSymbol(), ((currentCell.getRightEdge().get() < 1) ? '|' : ' '));
 		}
+
+		System.out.printf("\n");
+	}
+	
+	private static void printWallLine(Maze maze, int row)
+	{
+		MazeCell currentCell;
 		
-		System.out.printf("\n\n");
+		System.out.printf("#");
+		
+		for(int column = 0; column < Constants.getMazeSize(); column++)
+		{
+			currentCell = maze.getMazeGrid()[row][column];
+
+			System.out.printf("%c%c", ((currentCell.getDownEdge().get() < 1) ? '-' : ' '), '#');
+		}
+
+		System.out.printf("\n");
 	}
 }
